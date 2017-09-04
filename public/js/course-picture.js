054c7589afce785c8b354b9a56c11c990cd53ed3
wincherster <402992977@qq.com>
2017-09-04 16:46:05 +0800

    1.2.0版本-完成添加课程封面展示

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,19 @@
 define(['jquery','template','util'],function($,template,util){
 	// 设置导航菜单选中
 	util.setMenu('/course/add');
+	// 获取课程id
+	var csId = util.qs('cs_id');
+	// 调用接口，查询数据(封面和信息)，渲染页面
+	$.ajax({
+		type:'get',
+		url:'/api/course/picture',
+		data:{cs_id:csId},
+		dataType:'json',
+		success:function(data){
+			// console.log(data);
+			// 渲染页面
+			var html = template('pictureTpl',data.result);
+			$('#pictureInfo').html(html);
+		}
+	})
 });
